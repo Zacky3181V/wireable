36986736e0cd2670c41c634a0dedf48f22c5b28e
Daniyar Alkulov <93855417+Zacky3181V@users.noreply.github.com>
2025-05-02 19:27:52 +0200

    Create method for adding peers to the file (#7)

--- generator/wg.go
+++ generator/wg.go
@@ -1,32 +1,23 @@
 package generator
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
+	"os"
 
+	"github.com/Zacky3181V/wireable/config"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/curve25519"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 func generateWireGuardKeys() (string, string, error) {
-	var privateKey [32]byte
-	_, err := rand.Read(privateKey[:])
+	privateKey, err := wgtypes.GeneratePrivateKey()
 	if err != nil {
 		return "", "", err
 	}
 
-	privateKey[0] &= 248
-	privateKey[31] &= 127
-	privateKey[31] |= 64
+	publicKey := privateKey.PublicKey()
 
-	var publicKey [32]byte
-	curve25519.ScalarBaseMult(&publicKey, &privateKey)
-
-	privateKeyB64 := base64.StdEncoding.EncodeToString(privateKey[:])
-	publicKeyB64 := base64.StdEncoding.EncodeToString(publicKey[:])
-
-	return privateKeyB64, publicKeyB64, nil
+	return privateKey.String(), publicKey.String(), nil
 }
 
 // @Summary Generate Wireguard configuration
@@ -40,14 +31,28 @@ func generateWireGuardKeys() (string, string, error) {
 // @Router /generate [get]
 func WireGuardHandler(c *gin.Context) {
 	privateKey, publicKey, err := generateWireGuardKeys()
+	wgAllocator := config.GetAllocator()
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Failed to generate keys"})
 		return
 	}
 
+	ip, err := wgAllocator.Allocate()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "No IPs available"})
+		return
+	}
+
+	if err := appendPeerToFile("peers.conf", publicKey, ip); err != nil {
+		c.JSON(500, gin.H{"error": "Failed to update peers.conf"})
+		return
+	}
+
+	serverPublicKey := config.GetServerPublicKey()
+
 	configTemplate := fmt.Sprintf(`[Interface]
 PrivateKey = %s
-Address = 10.0.0.2/24
+Address = %s/24
 DNS = 1.1.1.1
 
 [Peer]
@@ -55,8 +60,25 @@ PublicKey = %s
 Endpoint = example.com:51820
 AllowedIPs = 0.0.0.0/0, ::/0
 PersistentKeepalive = 25
-`, privateKey, publicKey)
+`, privateKey, ip, serverPublicKey)
 
 	c.Header("Content-Type", "text/plain")
 	c.String(200, configTemplate)
+}
+
+func appendPeerToFile(filename, publicKey, ip string) error {
+	entry := fmt.Sprintf(`
+[Peer]
+PublicKey = %s
+AllowedIPs = %s/32
+`, publicKey, ip)
+
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(entry)
+	return err
 }
